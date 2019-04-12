0666c8fa8607058b4fb283719f553efa323ed49c
Lianzhen Fu <fu000048@algonquinlive.com>
2019-04-12 15:07:06 -0400

    setup again for ajax

--- js/dev/main.js
+++ js/dev/main.js
@@ -14801,3 +14801,6 @@ return jQuery;
 
 console.log('JS works good')
 console.log('gulpfile works')
+$(document).ready(function ($) {
+  $('#productHtml').load('products.html')
+})
--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 console.log('JS works good')
 console.log('gulpfile works')
+$(document).ready(function ($) {
+  $('#productHtml').load('products.html')
+})
