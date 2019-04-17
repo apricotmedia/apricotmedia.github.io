d047a6f716e5e3024a2227c20589d2927855b491
Nelvin Joy <joy00027@algonquinlive.com>
2019-04-17 15:07:40 -0400

    added working collapse

--- js/dev/main.js
+++ js/dev/main.js
@@ -14801,3 +14801,17 @@ return jQuery;
 
 console.log('JS works good')
 console.log('gulpfile works')
+$(document).ready(function($) {
+$('button.btn').click(function(){
+  var e = $(this).attr('data-toggle')
+  $(this).fadeOut(500).fadeIn(500)
+})
+
+
+
+
+
+
+
+
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 console.log('JS works good')
 console.log('gulpfile works')
+$(document).ready(function($) {
+$('button.btn').click(function(){
+  var e = $(this).attr('data-toggle')
+  $(this).fadeOut(500).fadeIn(500)
+})
+
+
+
+
+
+
+
+
+}
